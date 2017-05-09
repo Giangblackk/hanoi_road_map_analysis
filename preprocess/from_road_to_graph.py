0ea2072f3bfafd792b25d72ef0238ad29ff6de87
Giangblackk <giangblackk@gmail.com>
2017-05-10 01:47:53 +0700

    [update] use graph middle attr instead of new list

--- preprocess/from_road_to_graph.py
+++ preprocess/from_road_to_graph.py
@@ -18,7 +18,7 @@ spatialRef = layer.GetSpatialRef()
 featureCount = layer.GetFeatureCount()
 print(featureCount)
 # layer.SetAttributeFilter("ONEWAY NOT IN ('yes', 'no','-1')")
-layer.SetAttributeFilter("ONEWAY NOT IN ('-1','no','yes')")
+# layer.SetAttributeFilter("ONEWAY IN ('-1','yes','no')")
 # get attribute list
 attributeList = []
 layerDefinition = layer.GetLayerDefn()
@@ -60,73 +60,48 @@ for feature in layer:
         for nodeidx in G.nodes_iter():
             if G.node[nodeidx]['lng'] == lastPoint[0] and G.node[nodeidx]['lat'] == lastPoint[1]:
                 lastNodeID = nodeidx
-#    print(firstNodeID,lastNodeID)
-#    middlePointList = []
-#    for j in range(1,pointCount-1):
-#        middlePointList.append((geometry.GetPoint(j)[0], geometry.GetPoint(j)[1]))
-#    if feature.GetField('ONEWAY') == '-1':
-#        roadList.append({'first':lastNodeID,'last':firstNodeID,'middle':middlePointList})
-#    elif feature.GetField('ONEWAY') == 'yes':
-#        roadList.append({'first':firstNodeID,'last':lastNodeID,'middle':middlePointList})
-#    else:
-#        roadList.append({'first':firstNodeID,'last':lastNodeID,'middle':middlePointList})
-#        roadList.append({'first':lastNodeID,'last':firstNodeID,'middle':middlePointList})
-    # check if last or first point in on other road
 ###############################################################################
-    for road in roadList:
-        if firstPoint in road['middle']:
-            # print('head_intersect')
-            firstRoadID = road['first']
-            lastRoadID = road['last']
-#            print(G.node[firstRoadID])
-#            print(G.node[lastRoadID])
-#            print(firstRoadID,lastRoadID)
-#            print(G.edges())
-#            print(G.neighbors(firstRoadID))
-#            print(G.neighbors(lastRoadID))
-            attributeDict = G[firstRoadID][lastRoadID]
-            G.remove_edge(firstRoadID, lastRoadID)
-            G.add_edge(firstRoadID, firstNodeID, attr_dict=attributeDict)
-            G.add_edge(firstNodeID, lastRoadID, attr_dict=attributeDict)
-            roadList.remove(road)
-        elif lastPoint in road['middle']:
-            # print('tail_intersect')
-            firstRoadID = road['first']
-            lastRoadID = road['last']
-#            print(firstRoadID,lastRoadID)
-#            print(G.edges())
-            attributeDict = G[firstRoadID][lastRoadID]
-            G.remove_edge(firstRoadID, lastRoadID)
-            G.add_edge(firstRoadID, lastNodeID, attr_dict=attributeDict)
-            G.add_edge(lastNodeID, lastRoadID, attr_dict=attributeDict)
-            roadList.remove(road)
+    edges = G.edges()
+    for edge in edges:
+        headID = edge[0]
+        tailID = edge[1]
+        middle = G[headID][tailID]['middle']
+        if firstPoint in middle:
+            attributeDict = G[headID][tailID]
+            G.remove_edge(headID, tailID)
+            G.add_edge(headID, firstNodeID, attr_dict=attributeDict)
+            G.add_edge(firstNodeID, tailID, attr_dict=attributeDict)
+        elif lastPoint in middle:
+            attributeDict = G[headID][tailID]
+            G.remove_edge(headID, tailID)
+            G.add_edge(headID, lastNodeID, attr_dict=attributeDict)
+            G.add_edge(lastNodeID, tailID, attr_dict=attributeDict)
 ###############################################################################
+    middlePointList = []
+    for j in range(1,pointCount-1):
+        middlePointList.append((geometry.GetPoint(j)[0], geometry.GetPoint(j)[1]))
     # create link
     if feature.GetField('ONEWAY') == '-1':
         G.add_edge(lastNodeID, firstNodeID)
         for attribute in attributeList:
             G[lastNodeID][firstNodeID][attribute] = feature.GetField(attribute) if feature.GetField(attribute) is not None else ''
+        G[lastNodeID][firstNodeID]['middle'] = middlePointList
     elif feature.GetField('ONEWAY') == 'yes':
         G.add_edge(firstNodeID, lastNodeID)
         for attribute in attributeList:
             G[firstNodeID][lastNodeID][attribute] = feature.GetField(attribute) if feature.GetField(attribute) is not None else ''
+        G[firstNodeID][lastNodeID]['middle'] = middlePointList
     else:
         G.add_edge(firstNodeID, lastNodeID)
         G.add_edge(lastNodeID, firstNodeID)
         for attribute in attributeList:
             G[firstNodeID][lastNodeID][attribute] = feature.GetField(attribute) if feature.GetField(attribute) is not None else ''
             G[lastNodeID][firstNodeID][attribute] = feature.GetField(attribute) if feature.GetField(attribute) is not None else ''
-    # add to roadList
-    middlePointList = []
-    for j in range(1,pointCount-1):
-        middlePointList.append((geometry.GetPoint(j)[0], geometry.GetPoint(j)[1]))
-    if feature.GetField('ONEWAY') == '-1':
-        roadList.append({'first':lastNodeID,'last':firstNodeID,'middle':middlePointList})
-    elif feature.GetField('ONEWAY') == 'yes':
-        roadList.append({'first':firstNodeID,'last':lastNodeID,'middle':middlePointList})
-    else:
-        roadList.append({'first':firstNodeID,'last':lastNodeID,'middle':middlePointList})
-        roadList.append({'first':lastNodeID,'last':firstNodeID,'middle':middlePointList})
+        G[firstNodeID][lastNodeID]['middle'] = middlePointList
+        G[lastNodeID][firstNodeID]['middle'] = middlePointList
+# remove middle properties
+for edge in G.edges_iter():
+    G[edge[0]][edge[1]].pop('middle')
 # check if 2 node same lat long
 lat = G.node[0]['lat']
 lng = G.node[0]['lng']
@@ -140,7 +115,7 @@ for i in G.nodes_iter():
         lng = G.node[i]['lng']
 print(sameCount)
 # nx.write_gexf(G,'./highway_line_singlepart.gexf')
-nx.write_gexf(G,'./highway_line_singlepart_0123.gexf')
+nx.write_gexf(G,'./highway_line_singlepart_new.gexf')
 # create links between nodes
 # add metadata of links
 # save graph
